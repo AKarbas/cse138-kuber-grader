1a4d024df988a4383d8788c3556ffa5ed668da79
Amin Karbas <mkarbasf@ucsc.edu>
2023-03-09 20:32:26 -0800

    Fix main

--- cmd/grade.go
+++ cmd/grade.go
@@ -11,7 +11,7 @@ func main() {
 	client := k8s.Client{}
 
 	for {
-		pods, err := client.ListPods("", "app=hello")
+		pods, err := client.ListPods("", map[string]string{"app": "hello"})
 		if err != nil {
 			panic(err.Error())
 		}
@@ -19,6 +19,14 @@ func main() {
 
 		for idx, pod := range pods.Items {
 			fmt.Printf("Index %d: name: %s\n", idx, pod.GetName())
+		}
+
+		addresses, err := client.ListPodAddresses("", map[string]string{"app": "hello"})
+		if err != nil {
+			panic(err.Error())
+		}
+		for idx, addr := range addresses {
+			fmt.Printf("Pod %d: %s\n", idx+1, addr)
 		}
 		time.Sleep(10 * time.Second)
 	}
